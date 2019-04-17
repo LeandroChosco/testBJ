9ba54afcbb809df94398b38cec7ed5725ec1b63a
Ale Lujan <alujan@energetika.com>
2019-04-17 18:19:03 -0500

    add girtignore

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 const base_url = 'http://18.222.106.238'
 const webSocket = 'ws://18.222.106.238'
-const webSocketPort = '20'
-const apiPort = '3000'
+const webSocketPort = '40'
+const apiPort = '4000'
  export default {
      base_url,
      webSocket,
