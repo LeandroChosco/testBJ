382da2828ff3242bec57294ef50f720642f5f086
Ale Lujan <alujan@energetika.com>
2019-05-20 12:05:26 -0500

    fix merge ale

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,17 @@
+<<<<<<< Updated upstream
 const base_url = 'http://3.19.108.94'
 const webSocket = 'ws://3.19.108.94'
 const webSocketPort = '40'
 const apiPort = '4000'
+=======
+var base_url = 'http://localhost'
+var base_url = 'http://3.19.108.94'
+var webSocket = 'ws://localhost'
+var webSocket = 'ws://3.19.108.94'
+var webSocketPort = '20'
+var apiPort = '4000'
+var apiPort = '3000'
+>>>>>>> Stashed changes
  export default {
      base_url,
      webSocket,
