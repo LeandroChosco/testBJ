08e3658741fbe22daafcbc04bd365330584c1e07
Luis Melendez <luis.zdn@gmail.com>
2019-05-20 11:52:11 -0500

    CAmbio para ebsocket

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 const base_url = 'http://18.222.106.238'
-const webSocket = 'ws://18.222.106.238'
+const webSocket = 'ws://prod.streaming.energetikadevelepment.com'
 const webSocketPort = '20'
 const apiPort = '3000'
  export default {
