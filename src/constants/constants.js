95069c6b532d5c60ddfe757b224b455f58aa4e35
Luis Melendez <luis.zdn@gmail.com>
2019-07-26 17:32:47 -0500

    Correccion api

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 
-const base_url = 'http://dev.apicams.cuajimalpa.energetikadevelepment.com'
-const webSocket = 'ws://dev.streaming.cuajimalpa.energetikadevelepment.com'
-const apiStream = 'http://dev.streaming.cuajimalpa.energetikadevelepment.com'
+const base_url = 'http://prod.apicams.cuajimalpa.energetikadevelepment.com'
+const webSocket = 'ws://prod.streaming.cuajimalpa.energetikadevelepment.com'
+const apiStream = 'http://prod.streaming.cuajimalpa.energetikadevelepment.com'
 const webSocketPort = '20'
 const apiPort = '3000'
  export default {
