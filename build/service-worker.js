6e194402964fcf7d651d8a0d4ed9e803c88d429d
Luis Melendez Marcial <luis@MacBook-Air-de-Luis.local>
2019-07-30 17:59:05 -0500

    build para despliege

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca22997796f77b7ee32bf76df120fb6b.js"
+  "/precache-manifest.a7bf7214dc15e9fabfdea0ed3b8570fe.js"
 );
 
 workbox.clientsClaim();
