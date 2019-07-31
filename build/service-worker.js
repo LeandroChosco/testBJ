afc09a6f34c618510e1a447913bc1191df2b729c
Luis Melendez <luis.zdn@gmail.com>
2019-07-31 13:11:25 -0500

    build para despliege

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7bf7214dc15e9fabfdea0ed3b8570fe.js"
+  "/precache-manifest.aa982650f389518436e4b170a360779a.js"
 );
 
 workbox.clientsClaim();
