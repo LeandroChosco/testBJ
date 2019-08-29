288f1c744b7626f9f888122da8bed857303b63f1
Luis Melendez <luis.zdn@gmail.com>
2019-08-29 17:41:38 -0500

    correccion de url de api

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ const base_url = window.location.hostname === 'localhost' ?
     'http://localhost' :
     window.location.hostname === 'cams.mh.dev.energetikadevelepment.com' ? 
     'http://dev.apicams.mh.dev.energetikadevelepment.com' :
-    'http://prod.streaming.energetikadevelepment.com';
+    'http://prod.apicams.energetikadevelepment.com';
 const webSocketPort = '20'
 const apiPort = '3000'
  export default {
