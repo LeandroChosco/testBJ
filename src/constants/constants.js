a3e2000748c6c24163c96f758181ef5227c61a9a
Luis Melendez <luis.zdn@gmail.com>
2019-08-30 05:01:33 +0000

    Update constants.js correct base_url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,8 @@
-
 const base_url = window.location.hostname === 'localhost' ?
     'http://localhost' :
     window.location.hostname === 'cams.mh.dev.energetikadevelepment.com' ? 
     'http://dev.apicams.mh.dev.energetikadevelepment.com' :
-    'http://prod.apicams.energetikadevelepment.com';
+    'http://prod.apicams.mh.energetikadevelepment.com';
 const webSocketPort = '20'
 const apiPort = '3000'
  export default {
