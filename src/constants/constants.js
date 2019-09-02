389e2134c021aa7b7f22170b023928dce23c3bb3
estebanaldana <dantes-_inferno@hotmail.com>
2019-09-02 17:52:00 -0500

    config apicams benito juares

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,8 @@
 const base_url = window.location.hostname === 'localhost' ?
     'http://localhost' :
-    window.location.hostname === 'cams.cuauhtemoc.dev.energetikadevelepment.com' ? 
-    'http://dev.apicams.cuahutemoc.dev.energetikadevelepment.com' :
-    'http://prod.apicams.cuahutemoc.energetikadevelepment.com';
+    window.location.hostname === 'cams.bj.dev.energetikadevelepment.com' ? 
+    'http://dev.apicams.bj.energetikadevelepment.com/' :
+    'http://prod.apicams.bj.energetikadevelepment.com';
 const webSocketPort = '20'
 const apiPort = '3000'
  export default {
