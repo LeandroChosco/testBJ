fe8a319c8704818d2af78d0b2da84fe5975865df
Luis Melendez <luis.zdn@gmail.com>
2019-11-04 13:42:33 -0600

    correccion de api

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ const base_url = window.location.hostname === 'localhost' ?
     'http://localhost' :
     window.location.hostname === 'cams.cuajimalpa.dev.energetikadevelepment.com' ? 
     'http://dev.apicams.cuajimalpa.dev.energetikadevelepment.com' :
-    'http://18.191.177.38';
+    'http://prod.apicams.cuajimalpa.energetikadevelepment.com';
 const webSocketPort = '20'
 const apiPort = '3000'
  export default {
