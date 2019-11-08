0d29fcb2feccb4e57c42601f8f7bc2e3d6d7cad0
Luis Melendez <luis.zdn@gmail.com>
2019-11-08 17:30:13 -0600

    fix api

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 const base_url = window.location.hostname === 'localhost' ?
     'http://localhost' :
-    window.location.hostname === 'cams.cuauhtemoc.dev.energetikadevelepment.com' ? 
+    window.location.hostname === 'adminc5cuau-dev.s3-website.us-east-2.amazonaws.com/' ? 
     'http://dev.apicams.cuahutemoc.dev.energetikadevelepment.com' :
     'http://prod.apicams.cuahutemoc.energetikadevelepment.com';
 const webSocketPort = '20'
