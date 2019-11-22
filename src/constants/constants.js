504e2c7b1aadaa08e7d16be1e6e82d64e2660d11
Luis Melendez <luis.zdn@gmail.com>
2019-11-22 12:41:06 -0600

    correcion chat

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,8 @@
-const base_url = window.location.hostname === 'localhost' ?
+const base_url = 'http://prod.apicams.cuajimalpa.energetikadevelepment.com';/*window.location.hostname === 'localhost' ?
     'http://localhost' :
     window.location.hostname === 'cams.cuajimalpa.dev.energetikadevelepment.com' ? 
     'http://dev.apicams.cuajimalpa.dev.energetikadevelepment.com' :
-    'http://prod.apicams.cuajimalpa.energetikadevelepment.com';
+    'http://prod.apicams.cuajimalpa.energetikadevelepment.com';*/
 const webSocketPort = '20'
 const apiPort = '3000'
  export default {
