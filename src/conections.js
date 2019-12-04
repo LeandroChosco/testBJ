05628e0dad6ed06e043116d3a767047abfe8566b
Luis Melendez <luis.zdn@gmail.com>
2019-12-04 11:17:55 -0600

    correccion popualte en load cams para carga rapida

--- src/conections.js
+++ src/conections.js
@@ -54,10 +54,10 @@ export default {
         return Axios.put(constants.base_url+':'+constants.apiPort+'/tickets/toClose',data)
     },
     dashboardCams:()=>{        
-        return Axios.get(constants.base_url+ ':1337/dashboard/cams')
+        return Axios.get(/* constants.base_url+ */'http://3.133.151.209:1337/dashboard/cams')
     },
     dashboardTickets:()=>{        
-        return Axios.get(constants.base_url+ ':1337/dashboard/tickets')
+        return Axios.get(/* constants.base_url+ */'http://3.133.151.209:1337/dashboard/tickets')
     },
     dashboardTotalRecognition:()=>{        
         return Axios.get(/* constants.base_url+ */'http://3.133.151.209:1337/dashboard/detected')
@@ -72,7 +72,7 @@ export default {
         return Axios.get(/* constants.base_url+ */'http://3.133.151.209:1337/dashboard/mood')
     },
     loadCams:()=>{        
-        return Axios.get(/* constants.base_url+ */'http://3.133.151.209:1337/cams?sort=num_cam asc&active=1&limit=-1')
+        return Axios.get(/* constants.base_url+ */'http://3.133.151.209:1337/cams?sort=num_cam asc&active=1&limit=1000&populate=false')
     },
     changeCamStatus:(id)=>{        
         return Axios.put(constants.base_url+ ':' +  constants.apiPort + '/control-cams/change-status/' + id)
