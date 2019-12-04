1e38896471b29321f08df59da741b4d4f72b7532
Luis Melendez <luis.zdn@gmail.com>
2019-12-04 14:11:11 -0600

    corecion para conxion

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
-const base_url = /*window.location.hostname === 'localhost' ?
+const base_url = window.location.hostname === 'localhost' ?
     'http://localhost' :
     window.location.hostname === 'cams.cuajimalpa.dev.energetikadevelepment.com' ? 
-    'http://dev.apicams.cuajimalpa.dev.energetikadevelepment.com' :*/
+    'http://dev.apicams.cuajimalpa.dev.energetikadevelepment.com' :
     'http://prod.apicams.cuajimalpa.energetikadevelepment.com';
 const webSocketPort = '20'
 const apiPort = '3000'
