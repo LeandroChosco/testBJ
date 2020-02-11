fb9bfd903d7eaf6f8b7e508128b3d794cc790b95
Julio Barrera <juliobarrera@MacBook-Pro-de-Julio.local>
2020-02-11 11:05:44 -0600

    nuevos endpoint para multimedia, y nuevos features sonido, enviar mensaje y loop cuadrantes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,8 @@
 const base_url = window.location.hostname === 'localhost' ?
     'http://localhost' :
     window.location.hostname === 'cams.bj.dev.energetikadevelepment.com' ? 
-    'http://dev.apicams.bj.energetikadevelepment.com/' :
-    'http://prod.apicams.bj.energetikadevelepment.com';
+    'http://dev.apicams.bj.dev.energetikadevelepment.com' :
+    'http://dev.apicams.bj.dev.energetikadevelepment.com';
 const webSocketPort = '20'
 const apiPort = '3000'
  export default {
