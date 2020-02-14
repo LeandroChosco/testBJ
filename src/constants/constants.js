23a58d158b67ec602e1e9812c6d8d0819e40f85a
Robin Beltran <rbeltranmontijo@gmail.com>
2020-02-14 10:19:42 -0600

    bug con barra matches en cuadrantes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,10 @@
 const base_url =
-  window.location.hostname === "localhost"
-    ? "http://localhost"
-    : window.location.hostname === "cams.bj.dev.energetikadevelepment.com"
-    ? "http://dev.apicams.bj.dev.energetikadevelepment.com"
-    : "http://prod.apicams.bj.energetikadevelepment.com";
+  // window.location.hostname === "localhost"
+  //   ? "http://localhost"
+  //   : window.location.hostname === "cams.bj.dev.energetikadevelepment.com"
+  //   ? "http://dev.apicams.bj.dev.energetikadevelepment.com"
+  // :
+  "http://prod.apicams.bj.energetikadevelepment.com";
 const webSocketPort = "20";
 const apiPort = "3000";
 export default {
