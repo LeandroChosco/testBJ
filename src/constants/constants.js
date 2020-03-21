ecb976c3711219757150d50747c6e9ffd9118d71
mac <mac@MacBook-Pro-de-mac.local>
2020-03-20 18:06:49 -0600

    API Sails BJ

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,9 +6,11 @@ const base_url =
   // :
   "http://prod.apicams.bj.energetikadevelepment.com";
 const webSocketPort = "20";
+const sails_url = "http://prod.adminc5.bj.sails.energetikadevelepment.com";
 const apiPort = "3000";
 export default {
   base_url,
+  sails_url,
   apiPort,
   webSocketPort
 };
