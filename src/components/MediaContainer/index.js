3d291508a7b0a51e2cd3f45ec175efe4539a6590
oljudi <oljudimark@gmail.com>
2020-05-26 18:51:44 -0500

    Descarga de video

--- src/components/MediaContainer/index.js
+++ src/components/MediaContainer/index.js
@@ -8,6 +8,7 @@ import { NOTFOUND } from 'dns';
 import { stat } from 'fs';
 import Clappr from 'clappr'
 import HlsPlayer from '../HlsPlayer';
+import Spinner from 'react-bootstrap/Spinner'
 
 class MediaContainer extends Component {
     
@@ -26,15 +27,30 @@ class MediaContainer extends Component {
                     src={(this.props.dns_ip?this.props.dns_ip:this.props.servidorMultimedia)+':'+constants.apiPort+'/'+ this.props.src} 
                     style={{width:'100%'}}
                     />
-
             :null}
-            {this.props.image?<img src={this.props.servidorMultimedia+':'+constants.apiPort+'/'+ this.props.src} style={{width:'100%'}} alt='img'/>:null}
+            {this.props.image? <img src={this.props.servidorMultimedia+':'+constants.apiPort+'/'+ this.props.src } style={{width:'100%'}} alt='img'/>:null}
             {this.props.hour}
         </Card>
         <Modal show={this.state.modal} onHide={()=>this.setState({modal:false})}>
             <Modal.Header closeButton>                      
-                <Button basic onClick={this._saveFile}><i className='fa fa-download'></i> Descargar </Button>
-                {this.props.hideDelete?null:<Button basic negative onClick={this._deleteFile}><i className='fa fa-trash'></i> Eliminar</Button>}
+            {
+                        this.state.loading ?
+                            <>
+                                <Button variant="primary" disabled>
+                                    <Spinner
+                                    as="span"
+                                    animation="grow"
+                                    size="sm"
+                                    role="status"
+                                    aria-hidden="true"
+                                    />
+                                    Descargando...
+                                </Button>
+                            </> :
+                            
+                        <Button basic onClick={this._saveFile}><i className='fa fa-download'></i> Descargar </Button> 
+                    }   
+
             </Modal.Header>
             <Modal.Body>
                 {/* {this.props.video?<video ref='element' autoPlay controls src={(this.props.dns_ip?this.props.dns_ip:constants.base_url)+':'+constants.apiPort+'/'+ this.props.src} style={{width:'100%'}}/>:null} */}
@@ -49,7 +65,8 @@ class MediaContainer extends Component {
     );
     }
 
-    componentDidMount(){        
+    componentDidMount() {     
+        // console.log(this.props )
         
         //console.log('url',(this.props.dns_ip?this.props.dns_ip:constants.base_url)+':'+constants.apiPort+'/'+ this.props.src , this.props.servidorMultimedia) 
        
@@ -58,24 +75,40 @@ class MediaContainer extends Component {
     componentWillUnmount(){
         
     }
-    _saveFile = () => {
-        //console.log("dns_ip: ", this.props.dns_ip)
-        setTimeout(async()=>{
+    _saveFile = async () => {
+        this.setState({
+            loading: true
+        })
             const response={
-                file:this.props.servidorMultimedia +":" +constants.apiPort +"/"+ this.props.src
+                file: this.props.dns_ip ?
+                    this.props.dns_ip + ":" + constants.apiPort + "/" + this.props.src :
+                    this.props.servidorMultimedia + ":" + constants.apiPort + "/" + this.props.src
             };
             const statusResponse = await fetch(response.file)
             console.log("status: ", statusResponse)
             if(statusResponse.status === 200){
-                window.saveAs(response.file,this.props.video?'video.mp4':this.props.image?'image.jpg':'file')
+                window.saveAs(response.file, this.props.video ? 'video.mp4' : this.props.image ? 'image.jpg' : 'file')
+                console.log(response.file)
+                if (response.file.includes('video_history')) {
+                    setTimeout(() => {
+                        console.log('Video History Saved')
+                        this.setState({loading:false})
+                    }, 8500);
+                } else {
+                    setTimeout(() => {
+                        this.setState({loading:false})
+                    }, 300);
+                }
             }else{
                 const responseHistory={
                     file:this.props.dns_ip +":" +constants.apiPort +"/"+ this.props.src
                 };
-                window.saveAs(responseHistory.file,this.props.video?'video.mp4':this.props.image?'image.jpg':'file')
+                window.saveAs(responseHistory.file, this.props.video ? 'video.mp4' : this.props.image ? 'image.jpg' : 'file')
+                setTimeout(() => {
+                    this.setState({loading:false})
+                }, 300);
             }
-        }, 1000);
-        //window.saveAs(this.props.src,this.props.video?'video.mp4':this.props.image?'image.jpg':'file')       
+
     }
 
     _deleteFile = () => {
