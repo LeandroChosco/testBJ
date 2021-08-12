ce5510c794df5094ca2c89ffbe10dab5d1b998d9
Karen Figueroa <kyfr14@gmail.com>
2021-08-12 11:25:33 -0600

    Apuntando a produccion

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,16 +1,16 @@
 // Produccion
 // const sails_url = 'http://prod.adminc5.bj.sails.energetikadevelepment.com';
-// const sails_url = "http://3.21.75.143";
-// const radar_backend = "http://radar-backend-dev.us-east-2.elasticbeanstalk.com/graphql";
+const sails_url = "http://apibj.energetikatechnology.com";
+const radar_backend = "http://radar-backend-dev.us-east-2.elasticbeanstalk.com/graphql";
 const express_url = "http://3.15.181.186";
 
-//Dev
-const sails_url = "http://18.191.101.241";
-//const sails_url = "http://localhost";
-const radar_backend = "http://radar-backend-test.us-east-1.elasticbeanstalk.com/graphql";
+// Dev
+// const sails_url = "http://18.191.101.241";
+// const sails_url = "http://localhost";
+// const radar_backend = "http://radar-backend-test.us-east-1.elasticbeanstalk.com/graphql";
 const webSocketPort = '20';
 const apiPort = "3000";
-const sailsPort = "1337";
+const sailsPort = "80";
 const ptzPort = "3001";
 
 export default {
