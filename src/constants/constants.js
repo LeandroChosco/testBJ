dd5d005bee9da57e7b1c87a29062cc06ad9b890b
leandro.rchosco <leandro.rchosco@gmail.com>
2022-02-24 10:32:49 -0600

    configuracion de url sails

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ import favicon from '../assets/images/icons/favicon.jpg'
 // Produccion
 // const sails_url = 'http://prod.adminc5.bj.sails.energetikadevelepment.com';
 // const sails_url = "http://3.21.75.143";
-const sails_url = "http://apibjdev.energetikatechnology.com"; //actual
+const sails_url = "https://bjapi.radarapptechnologies.com"; //actual
 const radar_backend = "http://radarbackendprod.radarapptechnologies.com/graphql";
 const express_url = "http://3.15.181.186";
 
@@ -11,8 +11,8 @@ const dashboard="http://95.216.37.253:6868/dashboard/iframes?user_id=1";
 const detialDashboard="http://95.216.37.253:6868/dashboard/iframes/"
 
 //Dev
-// const sails_url = "http://adminc5dockerbj.us-east-1.elasticbeanstalk.com";
-// const sails_url = "http://localhost";
+// sails_url genera token de autenticacion 
+// const sails_url = "http://apibjdev.energetikatechnology.com/";
 // const radar_backend = "https://radarbackendtest.energetikadevelepment.com/graphql";
 const webSocketPort = '20';
 const apiPort = "3000";
